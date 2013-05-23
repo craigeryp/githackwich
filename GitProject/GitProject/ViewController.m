d257bab2692c65425650962c9ac42dd64b1d3afd
Craig Pellegrini <craig.pellegrini@live.com>
2013-05-23 11:31:05 -0500

    more changes

--- GitProject/GitProject/ViewController.m
+++ GitProject/GitProject/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"this is a major code change");
+    NSLog(@"forcing an error");
 }
 
 - (void)didReceiveMemoryWarning
