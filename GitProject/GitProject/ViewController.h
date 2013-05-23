f9cc11c42bfb44d5132afeeeca74a1ede0907c2f
Craig Pellegrini <craig.pellegrini@live.com>
2013-05-23 10:25:05 -0500

    first commit

---
+++ GitProject/GitProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitProject
+//
+//  Created by Craig on 5/23/13.
+//  Copyright (c) 2013 craigeryp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
